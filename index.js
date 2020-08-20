f62389b8a537d4efba474fa8f32837b4e8035935
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-20 11:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    returns `${this.name} saus meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name; 
+    this.sex = sex;
+  }
+  speak() {
+    returns `${this.name} saus woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    returns this.sex === 'male'? `It's me! ${this.name} the parrot!`: `${name} says squawk!`;
+  }
+}
